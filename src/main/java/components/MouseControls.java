993a955754596527e83a743581b04eebeef0e204
mark.exe-web <markatamergeorge@gmail.com>
2025-01-02 18:16:13 +0200

    finished plus fixed some bugs

--- src/main/java/components/MouseControls.java
+++ src/main/java/components/MouseControls.java
@@ -1,34 +1,54 @@
 package components;
 
 import Laevis.GameObject;
+import Laevis.KeyListener;
 import Laevis.MouseListener;
 import Laevis.Window;
 import LaevisUtilities.Settings;
+import org.joml.Vector4f;
 
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_ESCAPE;
 import static org.lwjgl.glfw.GLFW.GLFW_MOUSE_BUTTON_LEFT;
 
 public class MouseControls extends Component {
     GameObject holdingObject = null;
+    private float debounceTime = 0.10f;
+    private float debounce = debounceTime;
 
     public void pickupObject(GameObject go) {
+        if (holdingObject != null) {
+            this.holdingObject.destroy();
+        }
         this.holdingObject = go;
+        this.holdingObject.getComponent(SpriteRenderer.class).setColor(new Vector4f(0.8f, 0.8f, 0.8f, 0.5f));
+        this.holdingObject.addComponent(new NonPickable());
         Window.getScene().addGameObjectToScene(go);
     }
 
     public void place() {
-        this.holdingObject = null;
+        GameObject newObj = this.holdingObject.copy();
+        newObj.getComponent(SpriteRenderer.class).setColor(new Vector4f(1,1,1,1));
+        newObj.removeComponent(NonPickable.class);
+        Window.getScene().addGameObjectToScene(newObj);
+
     }
 
     @Override
     public void update(float dt) {
-        if (holdingObject != null) {
+        debounce -=dt;
+        if (holdingObject != null && debounce <= 0) {
             holdingObject.transform.position.x = MouseListener.getOrthoX();
             holdingObject.transform.position.y = MouseListener.getOrthoY();
-            holdingObject.transform.position.x = (int)(holdingObject.transform.position.x / Settings.GRID_WIDTH) * Settings.GRID_WIDTH;
-            holdingObject.transform.position.y = (int)(holdingObject.transform.position.y / Settings.GRID_HEIGHT) * Settings.GRID_HEIGHT;
+            holdingObject.transform.position.x = ((int)Math.floor(holdingObject.transform.position.x / Settings.GRID_WIDTH) * Settings.GRID_WIDTH) + Settings.GRID_WIDTH /2.0f;
+            holdingObject.transform.position.y = ((int)Math.floor(holdingObject.transform.position.y / Settings.GRID_HEIGHT) * Settings.GRID_HEIGHT) + Settings.GRID_HEIGHT /2.0f;
 
             if (MouseListener.mouseButtonDown(GLFW_MOUSE_BUTTON_LEFT)) {
                 place();
+                debounce = debounceTime;
+            }
+            if(KeyListener.isKeyPressed(GLFW_KEY_ESCAPE)){
+                holdingObject.destroy();
+                holdingObject = null;
             }
         }
     }
